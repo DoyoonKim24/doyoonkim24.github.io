1d4424aad8b04d895a6fc1715f269d3a9dff5ea6
DoyoonKim24 <doyoonkim424@gmail.com>
2024-06-25 23:46:26 -0400

    Header, hero, and homepage

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
-
+import doyoonLogo from './images/doyoon-logo.png';
+import listenMorePhoto from './images/listenMore.png';
+import holdingSign from './images/holding-sign.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons'
+import { faReact, faFigma} from '@fortawesome/free-brands-svg-icons'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav className="navbar">
+        <img src={doyoonLogo} alt="Doyoon Face Logo" height="32"/>
+        <div className="nav-group">
+          <a>Projects</a>
+          <a>Work</a>
+          <a>Skills</a>
+          <a>About</a>
+        </div>
+      </nav>
+      <div className='hero-section'>
+        <h4>Doyoon Kim</h4>
+        <h1 className='hero-header'>Software Developer <br/>and Designer</h1>
+      </div>
+
+      <div className='projects-section'>
+        <h2>Personal Projects</h2>
+        <div className='projects-flex'>
+          <div className='project'>
+            <div className='project-info'>
+              <h3>ListenMore</h3>
+              <div className='tech-used-group'>
+                <div className='tech-button'>
+                  <FontAwesomeIcon icon={faReact} />
+                  <h6 className='tech-button-text'>React</h6>
+                </div>
+                <div className='tech-button'>
+                  <FontAwesomeIcon icon={faFigma} />
+                  <h6 className='tech-button-text'>Figma</h6>
+                </div>
+              </div>
+            </div>
+            <div className='image-container'>
+              <img className='product-image' src={listenMorePhoto} alt="Photo of ListenMore App"/>
+            </div>
+            <div className='arrow-container'>
+              <FontAwesomeIcon className='arrow' icon={faArrowRight} size='xl'/>
+            </div>
+          </div>
+          <div className='project'>
+            <div className='project-info'>
+              <h3>ListenMore</h3>
+              <div className='tech-used-group'>
+                <div className='tech-button'>
+                  <h6>React</h6>
+                </div>
+                <div className='tech-button'>
+                  <h6>Figma</h6>
+                </div>
+              </div>
+            </div>
+            <div className='image-container'>
+              <img className='product-image' src={listenMorePhoto} alt="Photo of ListenMore App"/>
+            </div>
+            <div className='arrow-container'>
+              <FontAwesomeIcon className='arrow' icon={faArrowRight} size='xl'/>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
