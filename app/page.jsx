1b2b186ae48ad234d6fee49078894f0e2f85927f
DoyoonKim24 <doyoonkim424@gmail.com>
2024-09-12 18:27:31 -0400

    image loading

--- app/page.jsx
+++ app/page.jsx
@@ -6,7 +6,7 @@ import Experience from "./home-sections/Experience";
 import Skills from "./home-sections/Skills";
 import About from "./home-sections/About";
 import Footer from "./components/Footer";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Image from "next/image";
 import landingIllustration from "../public/images/landing-illustration.png";
 import spinny from "../public/images/spinning.png";
@@ -14,6 +14,8 @@ import heroHello from "../public/images/hero-hello.png";
 import { useInView } from 'react-intersection-observer';
 
 function App() {
+  const [imagesLoaded, setImagesLoaded] = useState(false);
+
   useEffect(() => {
     const handleSmoothScroll = (event) => {
       event.preventDefault();
@@ -41,36 +43,65 @@ function App() {
 
   const { ref: ref1, inView: inView1 } = useInView({
     threshold: 0.5,
-    triggerOnce: true
+    triggerOnce: true,
   });
 
   const { ref: ref2, inView: inView2 } = useInView({
     threshold: 0.5,
-    triggerOnce: true
+    triggerOnce: true,
   });
 
+  // This function checks if all images are loaded
+  const handleImageLoad = () => {
+    setImagesLoaded(true);
+  };
 
   return (
     <div className="App">
-      <Navbar />
-      <div className="hero-section" id="home">
-        <Image 
-          className={`hero-header ${inView1 ? 'fade-in' : 'fade-out'}`} 
-          src={heroHello} 
-          alt="Hello, I'm Doyoon!"
-          ref={ref1}
-        />
-        <div className={`hero-images ${inView2 ? 'fade-in' : 'fade-out'}`} style={{transition: '0.5s', transitionDelay: '0.1s'}} ref={ref2}>
-          <Image className='hero-drawing' src={landingIllustration} alt="landing image" width={0} height={0} style={{ width: '100%', height: 'auto' }}/>
-          <Image className='spinny' src={spinny} alt="software developer and designer spinning icon"/>
-        </div>
-      </div>
+      {!imagesLoaded && null
+      }
 
-      <Projects />
-      <Experience />
-      <Skills />
-      <About />
-      <Footer />
+      {imagesLoaded && (
+        <>
+          <Navbar />
+          <div className="hero-section" id="home">
+            <Image
+              className={`hero-header ${inView1 ? 'fade-in' : 'fade-out'}`}
+              src={heroHello}
+              alt="Hello, I'm Doyoon!"
+              ref={ref1}
+              onLoad={handleImageLoad}
+            />
+            <div
+              className={`hero-images ${inView2 ? 'fade-in' : 'fade-out'}`}
+              style={{ transition: '0.5s', transitionDelay: '0.1s' }}
+              ref={ref2}
+            >
+              <Image
+                className="hero-drawing"
+                src={landingIllustration}
+                alt="landing image"
+                width={0}
+                height={0}
+                style={{ width: '100%', height: 'auto' }}
+                onLoad={handleImageLoad}
+              />
+              <Image
+                className="spinny"
+                src={spinny}
+                alt="software developer and designer spinning icon"
+                onLoad={handleImageLoad}
+              />
+            </div>
+          </div>
+
+          <Projects />
+          <Experience />
+          <Skills />
+          <About />
+          <Footer />
+        </>
+      )}
     </div>
   );
 }
